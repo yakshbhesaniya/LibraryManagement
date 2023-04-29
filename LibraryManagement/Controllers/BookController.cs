7e8882d82bc24a5ffff05a85c3a82b3d831973c1
nilay_d <niayniay99@gmail.com>
2023-04-29 11:40:02 +0530

    Book Controller

--- LibraryManagement/Controllers/BookController.cs
+++ LibraryManagement/Controllers/BookController.cs
@@ -14,6 +14,7 @@ namespace LibraryManagement.API.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+            // book controller get all.
         }
 
         // GET api/<BookController>/5
